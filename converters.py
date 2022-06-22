5f7c69ad979030cc234b0e40e7635f2d02e330f7
Dima Mayor <71175311+NonExistentUsername@users.noreply.github.com>
2022-06-22 12:40:10 +0300

    Fixed BoostedConverter

--- converters.py
+++ converters.py
@@ -93,63 +93,6 @@ class BoostedConverter(Converter):
             result.append((key, tmp_result[key]))
         return result
 
-class BoostedConverter(Converter):
-    DELETED_SYMBOL = '~'
-
-    def __init__(self, alphabet: list[str]) -> None:
-        super().__init__()
-        if len(alphabet) == 0:
-            raise ValueError()
-        
-        self.__alphabet = []
-        tmp = set()
-        for key in alphabet:
-            if not key in tmp:
-                self.__alphabet.append(key)
-                tmp.add(key)
-
-    def __z_func(self, substr: list) -> int:
-        n = len(self.__data)
-        m = len(substr)
-        for i in range(m):
-            self.__data[i] = substr[i]
-
-        l = 0
-        r = 0
-        cnt = 0
-        # for i in range(1, n):
-        #     if i <= r:
-        #         z[i] = min(r - i + 1, z[i - l])
-        #     while i + z[i] < n and self.__data[z[i]] == self.__data[i + z[i]]:
-        #         z[i] += 1
-        #     if i + z[i] - 1 > r:
-        #         l = i
-        #         r = i + z[i] - 1
-
-        # stop_clear_pos = 0
-        # for i in range(m + 1, n):
-        #     if stop_clear_pos > i:
-        #         self.__data[i] = BoostedConverter.DELETED_SYMBOL
-        #     elif z[i] >= m:
-        #         self.__data[i] = BoostedConverter.DELETED_SYMBOL
-        #         stop_clear_pos = i + m
-        #         cnt += 1
-
-        return cnt
-
-    def __call__(self, data: str) -> list:
-        self.__data = split(BoostedConverter.DELETED_SYMBOL * (len(self.__alphabet[0]) + 1) + data)
-        # self.__z = [0] * len(self.__data)
-        tmp_result = {}
-        sorted_alphabet = self.__alphabet.copy()
-        sorted_alphabet.sort(key=lambda x: len(x), reverse=True)
-        for substr in sorted_alphabet:
-            tmp_result[substr] = self.__z_func(split(substr))
-        result = []
-        for key in self.__alphabet:
-            result.append((key, tmp_result[key]))
-        return result
-
 class BoostedConverter2(Converter):
     DELETED_SYMBOL = '~'
 
