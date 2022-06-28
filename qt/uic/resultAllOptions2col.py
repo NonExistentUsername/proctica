09e107584e045ceafaef3fd17fa047d1d679419b
Master <petrusvitaliywork@gmail.com>
2022-06-28 14:58:36 +0300

    upd

--- qt/uic/resultAllOptions2col.py
+++ qt/uic/resultAllOptions2col.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(412, 453)
+        Form.resize(412, 432)
         Form.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.verticalLayout = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout.setObjectName("verticalLayout")
